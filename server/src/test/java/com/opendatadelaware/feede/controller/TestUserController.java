f082d735ecd8eee6a312085a77b187a42b69b5a0
Dennis Kalaygian <daeniskalaygian@gmail.com>
2017-06-29 11:20:31 -0400

    Working on the JSON deserialization

--- server/src/test/java/com/opendatadelaware/feede/controller/TestUserController.java
+++ server/src/test/java/com/opendatadelaware/feede/controller/TestUserController.java
@@ -3,6 +3,7 @@ package com.opendatadelaware.feede.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.opendatadelaware.feede.dao.UsersDao;
 import com.opendatadelaware.feede.model.Users;
+import org.codehaus.jackson.type.TypeReference;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,6 +21,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Base64;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -47,18 +50,29 @@ public class TestUserController {
   @Test
   public void testPutBadInput() throws Exception {
     String badAuth = "eyIiOiIiLCJoZXkiOiJ3aGF0c3VwIiwibm8iOiJwcm9mYW5pdHkiLCJkZW5uaXMiOiJtYXJpb2thcnRtYXN0ZXIifQ==";
-    Map<String, String> badInput = new HashMap(); //"{\" : %s}", badAuth);
-    this.mvc.perform(post("/api/user")
-            .contentType(MediaType.APPLICATION_JSON).content(badInput))
-              .andExpect(status().isBadRequest());
+    Map<String, String> badInput = Collections.singletonMap("auth", badAuth);
+    byte[] jsonData = Files.readAllBytes(Paths.get(badInput.get("auth")));
+    Base64.Decoder decoder = Base64.getDecoder();
+    byte[] mapData = decoder.decode(jsonData);
+    Map<String,String> objectMap = new HashMap<String, String>();
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMap = objectMapper.readValue(mapData, HashMap.class);
+
+    /*this.mvc.perform(post("/api/user")
+            .contentType(MediaType.APPLICATION_JSON).content(badInput.get("auth")))
+              .andExpect(status().isBadRequest());*/
   }
 
   @Test
   public void testPutValidInput() throws Exception {
-      byte[] jsonData = Files.readAllBytes(Paths.get("GoodInputJson.json"));
+      byte[] jsonAsBytes = Files.readAllBytes(Paths.get("GoodInputJson.json"));
+      String jsonAsString = new String(jsonAsBytes);
       //String validAuth = "eyJmaXJzdE5hbWUiOiJEZW5uaXMiLCJsYXN0TmFtZSI6IkthbGF5Z2lhbiIsImVtYWlsIjoiZGVubmlza2FsYXlnaWFuQGdtYWlsLmNvbSIsInBhc3N3b3JkIjoiMTIzNDUiLCJwaG9uZSI6IjMwMjMzMzMzMzMiLCJzdHJlZXQiOiI4MTEgQmlyY2ggQXZlIiwiY2l0eSI6Ik1pbGZvcmQiLCJzdGF0ZSI6IkRFIiwiemlwIjoiMTk5NjMifQ==";
       ObjectMapper objectMapper = new ObjectMapper();
-      Users user = objectMapper.readValue(jsonData, Users.class);
+      TypeReference<HashMap<String, String>> typeRef = new TypeReference<HashMap<String,String>>() {};
+      Map<String, String> user = objectMapper.readValues(jsonAsString, new HashMap<String,String>);//typeRef);
+      //String jsonValue = o
+
 
   }
 }
